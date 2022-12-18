e69127afda6a305e3de6012954b06487d5aa687f
Jamison Fox <fox.jamison@outlook.com>
2022-12-18 10:44:12 -0700

    initial commit and configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <img src='https://th.bing.com/th/id/OIP.TnnO-9C6THhBBCzOhTe7mQHaFj?pid=ImgDet&rs=1'/>
     </div>
   );
 }
